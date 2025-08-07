0c7b5f75794c170f75f8b4c49a38ac1b0ccc5335
ISTKHAR-king <istkharalam225@gmail.com>
2025-08-08 00:27:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ISTKHAR-king/AnonMusic")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
 
